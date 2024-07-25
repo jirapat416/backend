15651c8360bd124ece28aeed98f7087ebb3d1f45
jirapat <jirapat416m@gmail.com>
2024-07-25 14:40:28 +0700

    Initial commit

--- app/api/route.js
+++ app/api/route.js
@@ -1,17 +1,26 @@
+import { Client } from 'pg';
+import dotenv from 'dotenv';
+dotenv.config();
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+});
+client.connect();
+
 export async function GET() {
     try {
-      return new Response(JSON.stringify({ message: "GET DATA OK"}), {
-        status: 200,
-        headers: { "Content-Type": "application/json" },
-      });
+          const result = await client.query('SELECT * FROM tbl_users');
+          return new Response(JSON.stringify(result.rows), {
+              status: 200,
+              headers: { "Content-Type": "application/json" },
+          });
     } catch (error) {
-      console.error(error);
-      return new Response(JSON.stringify({ error: "Internal Server Error" }), {
-        status: 500,
-        headers: { "Content-Type": "application/json" },
-      });
+      
+          return new Response(JSON.stringify({ error: "Internal Server Error" }), {
+              status: 500,
+              headers: { "Content-Type": "application/json" },
+          });
     }
-}
+  }
 
 export async function POST() {
     try {
